7563f85b939091276da7144b18368a4b6f7209a5
roni9843 <ronironi98439843@gmail.com>
2026-01-02 16:25:17 +0600

    added 3

--- src/main.jsx
+++ src/main.jsx
@@ -4,10 +4,10 @@ import { BrowserRouter } from 'react-router-dom';
 import AppRoutes from './routes/AppRoutes.jsx';
 import './index.css';
 
-// Root React render with React Router
+// Root React render with React Router and GitHub Pages basename
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/sonakanda-client/"> {/* <-- Important for GH Pages */}
       <AppRoutes />
     </BrowserRouter>
   </React.StrictMode>
